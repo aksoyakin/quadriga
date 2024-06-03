cf5519ed2adbb1cfbbcfcbb0530d641d960cba3e
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-06-03 18:53:20 +0300

    create java spring and angular projects

---
+++ quadriga-be/src/main/java/dev/akinaksoy/quadrigabe/QuadrigaBeApplication.java
@@ -0,0 +1,13 @@
+package dev.akinaksoy.quadrigabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadrigaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadrigaBeApplication.class, args);
+    }
+
+}
---
+++ quadriga-be/src/test/java/dev/akinaksoy/quadrigabe/QuadrigaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akinaksoy.quadrigabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadrigaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
