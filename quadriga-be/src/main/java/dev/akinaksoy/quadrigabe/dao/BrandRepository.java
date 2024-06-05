74507d399160594c567553321ec88009a15dd76d
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-06-05 16:23:07 +0300

    update BrandRepository

---
+++ quadriga-be/src/main/java/dev/akinaksoy/quadrigabe/dao/BrandRepository.java
@@ -0,0 +1,14 @@
+package dev.akinaksoy.quadrigabe.dao;
+
+import dev.akinaksoy.quadrigabe.entities.concretes.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+    boolean existsByName(String name);
+    List<Brand> findByName(String name);
+    List<Brand> findByNameContaining(String name);
+    List<Brand> findByNameStartingWith(String name);
+    List<Brand> findByNameEndingWith(String name);
+}
